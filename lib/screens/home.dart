2381979fb9be26c2ec9b51bc7fdc634bf831c2b8
Ali <ozdemirali@hotmail.com>
2022-04-10 23:41:10 +0300

    Home Page is started.

---
+++ lib/screens/home.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:student_follow_app/widgets/listStudent.dart';
+import 'package:student_follow_app/widgets/setting.dart';
+
+class Home extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return HomeState();
+  }
+}
+
+class HomeState extends State<Home> with SingleTickerProviderStateMixin {
+  static const List<Tab> myTabs=<Tab>[
+    Tab(icon:Icon(Icons.home)),
+    Tab(icon:Icon(Icons.settings))
+  ];
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _tabController=new TabController(length: myTabs.length,initialIndex: 0, vsync:this);
+  }
+
+@override
+void dispose(){
+    _tabController.dispose();
+    super.dispose();
+}
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Admin",style:TextStyle(fontSize: 20.0,fontWeight: FontWeight.normal)),
+        bottom: TabBar(
+          controller: _tabController,
+          indicatorColor: Colors.white,
+          tabs: myTabs,
+        ),
+        actions: [
+          TextButton(
+            style: TextButton.styleFrom(
+              textStyle: const TextStyle(fontSize: 20,fontWeight: FontWeight.normal),
+              primary: Colors.white
+            ),
+              onPressed: (){
+                print("Çıkış");
+              },
+              child: Text("Çıkış")),
+        ],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          ListStudent(),
+          Setting()
+        ],
+      ),
+    );
+  }
+}
