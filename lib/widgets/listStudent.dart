0abe962a2059bb2dfe1bc6b50469b52457b4b638
Ali <ozdemirali@hotmail.com>
2022-04-14 23:46:59 +0300

    StudentController is done

--- lib/widgets/listStudent.dart
+++ lib/widgets/listStudent.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:student_follow_app/models/student.dart';
+import 'package:student_follow_app/services/studentController.dart';
 
 class ListStudent extends StatefulWidget{
   @override
@@ -9,32 +11,58 @@ class ListStudent extends StatefulWidget{
 }
 
 class ListStudentState extends State<ListStudent> {
-  final List<String> entries = <String>['A', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C', 'B', 'C'];
+
+  late Future<List<Student>> students;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    students=StudentController().getFilterEssential();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Container(
       padding: EdgeInsets.fromLTRB(0, 2, 0, 0),
-      child: ListView.builder(
-          shrinkWrap: true,
-          itemCount: entries.length,
-          itemBuilder: (BuildContext context,int position){
-            return Card(
-              color: Colors.blue,
-              elevation: 2.0,
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor:Colors.white ,
-                  child: Text("A"),
-                ),
-                title: Text("Aloo"),
-                subtitle: Text("Deneme"),
-                onTap: (){
-                  print("Seçildi");
-                },
-              ),
-            );
-          }),
+      child:FutureBuilder<List<Student>>(
+        future: students,
+        builder: (context,snapshot){
+          if(snapshot.hasData){
+            print(snapshot.data!.length);
+            return   ListView.builder(
+                shrinkWrap: true,
+                itemCount: snapshot.data!.length,
+                itemBuilder: (BuildContext context,int position){
+                  return Card(
+                    color: Colors.blue,
+                    elevation: 2.0,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundColor:Colors.white ,
+                        child: Text("A"),
+                      ),
+                      title: Text(snapshot.data![position].nameAndSurname),
+                      subtitle: Text(snapshot.data![position].classroom + " - " +
+                                      snapshot.data![position].number + " - " +
+                                      snapshot.data![position].phone
+                      ),
+                      onTap: (){
+                        print("Seçildi");
+                      },
+                    ),
+                  );
+                });
+          }
+          else{
+            return Text("Veri Gelmedi");
+          }
+        },
+      ),
+
+
+
     );
   }
 }
